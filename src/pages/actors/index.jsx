d04991c3e5c8c2eb31e2dcdb50da07b0f78523d0
asijanah <asafjanah@gmail.com>
2020-11-29 14:02:11 +0100

    fix img url and add list of movies to actors

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -13,6 +13,7 @@ function People() {
         console.log(mostPopular);
     }, []);
 
+    const imgAddress='https://image.tmdb.org/t/p/w500'
     return (
 
         <div className="actors-card">
@@ -20,25 +21,24 @@ function People() {
                 mostPopular && mostPopular.map(item =>
                     <div className="actors">
                         <div className="actor-img">
-                            <img src={item.profile_path} />
+                            <img src={`${imgAddress}${item.profile_path}`} />
                         </div>
-                        
+                        <h2 className="actor-name">{item.name}</h2>
                         <ul>
                             {item.known_for.map(movie => {
-                                <li>{movie.title}</li>
-
+                                console.log(`${movie.title}`);
+                                return <li>{movie.title}</li>
                             })}
                         </ul>
-                        <h2 className="actor-name">{item.name}</h2>
                         <footer className="actor-popularity">
                             <h5>Actor Rating: {item.popularity}</h5>
                         </footer>
                     </div>
                 )
-            }
+    // https://image.tmdb.org/t/p/w500/3JTEc2tGUact9c0WktvpeJ9pajn.jpg
 
+            }
         </div>
-        // <div>{!mostPopular && mostPopular[0].name}</div>
     )
 }
 export default People
