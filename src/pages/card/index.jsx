5b23d06ccc75fd7a0d018a9957bdc74accdd217a
asijanah <asafjanah@gmail.com>
2020-11-29 14:07:56 +0100

    edit class names to meaningfull

--- src/pages/card/index.jsx
+++ src/pages/card/index.jsx
@@ -1,37 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import {useParams, useHistory} from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './card.css'
 
 
 function Card(props) {
     return (
-        <div className="maincard">
-            <div className="cards">
-                <div className="cardz">
-                    <Link to={`/movie/${props.movie.id}`}>
-                    <img className="image" src={`${props.movie.base_url}/${props.movie.logo_sizes[3]}/${props.movie.poster_path}`} />
-                    </Link>
-                    <p className="categoryz">Genre</p>
-                    <div className="card-contant">
-                        <h2 className="title">{props.movie.title}</h2>
-                        <p className="year">Released: {props.movie.release_date}</p>
-                        <footer className="foot">
-                            <div className="meta">
-                                <span className="duration"><i className="fa fa-clock-o"></i>duration:</span>
-                                <span className="views"><i className="fa fa-comments"></i><a href="#">views rateing:{props.movie.popularity}</a></span>
-                            </div>
-                        </footer>
-                    </div>
+        <div className="main-cards">
+            <div className="card">
+                <Link to={`/movie/${props.movie.id}`}>
+                    <img className="image" src={`${props.movie.base_url}/${props.movie.logo_sizes[2]}/${props.movie.poster_path}`} />
+                </Link>
+                <p className="categories">Genre</p>
+                <div className="card-contant">
+                    <h2 className="title">{props.movie.title}</h2>
+                    <p className="year">Released: {props.movie.release_date}</p>
+                    <footer className="foot">
+                        <div className="meta">
+                            <span className="duration"><i className="fa fa-clock-o"></i>duration:</span>
+                            <span className="views"><i className="fa fa-comments"></i><a href="#">views rateing:{props.movie.popularity}</a></span>
+                        </div>
+                    </footer>
                 </div>
             </div>
-
-
-            {/* <span>{`${props.movie.id}:  ${props.movie.title}`}
-            </span>
-            <img src={`${props.conf.base_url}/${props.conf.logo_sizes[1]}/${props.movie.poster_path}`}
-                alt={props.movie.title} /> */}
         </div>
     )
 }
