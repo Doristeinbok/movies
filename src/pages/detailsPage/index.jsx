5eb83d9fed4a2b2c992f7f369ff20445eaff323f
Doristeinbok <Doristeinbok@gmail.com>
2020-11-30 10:46:04 +0200

    add link for reloading detailsPage with new movie

--- src/pages/detailsPage/index.jsx
+++ src/pages/detailsPage/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {useParams, useHistory, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import Button from '@material-ui/core/Button';
 
 import './style.css';
@@ -7,18 +8,14 @@ import { actorDetails, recomendations, getOneMovie, getTMDBconf, gerCredits} fro
 import { getOMDB } from '../../api/OMDB';
 
 
-function MovieDetails (props) { 
+function MovieDetails () { 
 
-    const {movieId}=useParams(); 
+    let {movieId}=useParams(); 
     const history = useHistory();
     
     // const goHome = () => {        
     //     history.push("/home");
     // }
-    
-    const [movie2Display, setMovie2Dsiplay] = useState(        
-        props.movies.find(movie => movie.id === parseInt(movieId))
-      );   
 
     const [movie, setMovie] = useState();
     const [confImg, setConfImg] = useState();
@@ -26,7 +23,7 @@ function MovieDetails (props) {
     const [rating, getRating] = useState();
     const [recomendedMovies, setRecomendedMovies] = useState();
 
-      useEffect(async () => {
+    useEffect(async () => {
         //get movie 
         const movieData = await getOneMovie(movieId);
         setMovie(movieData);
@@ -46,26 +43,28 @@ function MovieDetails (props) {
         // console.log(imgConf);
 
         //get actors from credits
-        const credits = await gerCredits(movieId);
+        console.log('movieId');
+        console.log(movieId);
+        let credits = await gerCredits(movieId);
+        // let newCredits = {...credits, 
+        //                 ...credits.data,
+        //                     id: movieId    
+        //                 };
+
+        console.log('movieId for the credits (actors)');
+        console.log(credits);
         setActor(credits);
-        // console.log('credits');
-        // console.log(credits);
 
         //show recomended movies
         const recomendedMoviesData = await recomendations(movieId);
-        console.log('recomendedMovies');
-        console.log(recomendedMoviesData);
+        // console.log('recomendedMovies');
+        // console.log(recomendedMoviesData);
         setRecomendedMovies(recomendedMoviesData);
-        console.log(recomendedMovies);
+        // console.log(recomendedMovies);
         // const recomendedMoviesUrl = recomendedMoviesData.data.results.map(movie => movie.backdrop_path);
 
-        //use this to find movies that the actor play in
-        // const actorsInMovie = movie2Display.OMDB.data.Actors.split(',');
-        // // console.log(movie2Display.OMDB.data);
-        // const actorsDetails = await Promise.all(actorsInMovie.map(actor => actorDetails(actor)));
-        // const actorsImg = actorsDetails.map(actor => actor.data.results[0].profile_path);
-
-    },[]);
+    },[movieId]);
+    
     return (
         <>
             <div className="head">
@@ -74,7 +73,7 @@ function MovieDetails (props) {
                     <div className="head-details"> 
                         <h1>{movie.data.title}</h1>
                         <h3 className="plot-summery">{movie.data.overview}</h3>
-                        {rating && rating.data.Ratings.map((rating, index) => <h3 key={index}>{rating.Source} : {rating.Value}</h3>)}
+                        {rating && !rating.data.Error && rating.data.Ratings.map((rating, index) => <h3 key={index}>{rating.Source} : {rating.Value}</h3>)}
                     </div>
                 }
                 {
@@ -91,8 +90,12 @@ function MovieDetails (props) {
         <div className="actors">
             {actor && 
                 actor.data.cast.map((actorUrl, index) => 
-                <img key={index} className="oneActor" 
-                src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${actorUrl.profile_path}`} alt='actor' />)
+                {return(
+                    actorUrl.profile_path && 
+                    <img key={index} className="oneActor" 
+                    src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${actorUrl.profile_path}`} alt={actorUrl.name} 
+                    />)
+                }).slice(0,5)
             }
         </div>
 
@@ -101,9 +104,15 @@ function MovieDetails (props) {
         <h1>You may like also...</h1>
         <div className="similarMovies">
             {recomendedMovies && recomendedMovies.data.results.map((movieUrl, index) => 
-            <img key={index} className="movieSim"  
-            src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${movieUrl.poster_path}`} alt='recomended movies'/>   
-            )}
+                <Route path="/movie/:movieId">
+                    <Link to={`/movie/${movieUrl.id}`}>
+                        <img key={index} className="movieSim"  
+                        src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${movieUrl.poster_path}`} 
+                        alt={movieUrl.title}
+                        />   
+                    </Link>
+                </Route>
+            ).slice(0,10)}
         </div>
 
         {/* <div className="buttons-group">
