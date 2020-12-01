4b0bc545c21ad66398da124f9b5141ca1b753d88
asijanah <asafjanah@gmail.com>
2020-12-01 23:50:42 +0100

    Add info to card from API

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -30,7 +30,7 @@ function MovieNav(props) {
                     defaultValue={props.search}
                     onChange={props.onSearch}
                     />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
--- src/pages/card/index.jsx
+++ src/pages/card/index.jsx
@@ -6,14 +6,15 @@ import './card.css'
 
 
 function Card(props) {
-    
     return (
         <div className="main-cards px-2">
             <div className="movie-card">
                 <Link to={`/movie/${props.movie.id}`}>
                     <img className="image" src={`${props.movie.base_url}/${props.movie.logo_sizes[2]}/${props.movie.poster_path}`} />
                 </Link>
-                <p className="categories">Genre</p>
+                {props.movie.OMDB &&
+                        <p className="categories">{props.movie.OMDB.data.Genre.split(",")[0]}</p>
+                }
                 <div className="card-contant">
                     <h3 className="title">{props.movie.title}</h3>
                     <p className="year">Released: {props.movie.release_date}</p>
