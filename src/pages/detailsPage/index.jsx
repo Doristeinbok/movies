dcc3c24c06f18e4bb9ab462f9bd7b9cf4050093c
Doristeinbok <Doristeinbok@gmail.com>
2020-12-02 09:42:58 +0200

    add actor's movies and some style

--- src/pages/detailsPage/index.jsx
+++ src/pages/detailsPage/index.jsx
@@ -1,92 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import {useParams, useHistory, Link} from 'react-router-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
+import {Route} from 'react-router-dom'; 
+
+import './style.css';
+import { actorDetails, recomendations, getOneMovie, getTMDBconf, getCredits, getMoviesForActor} from '../../api/TMDB';
+import { getOMDB } from '../../api/OMDB';
+
+//play with material-ui
 import Button from '@material-ui/core/Button';
-import RateReviewIcon from '@material-ui/icons/RateReview';
-import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
 import Tooltip from '@material-ui/core/Tooltip';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import Alert from '@material-ui/lab/Alert';
-
-import './style.css';
-import { actorDetails, recomendations, getOneMovie, getTMDBconf, gerCredits} from '../../api/TMDB';
-import { getOMDB } from '../../api/OMDB';
-
+//icons
+import RateReviewIcon from '@material-ui/icons/RateReview';
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+//fonts
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
 
 function MovieDetails () { 
 
     let {movieId}=useParams(); 
     const history = useHistory();
     
-    // const goHome = () => {        
-    //     history.push("/home");
-    // }
+    const goHome = () => {        
+        history.push("/home");
+    }
 
     const [movie, setMovie] = useState();
     const [confImg, setConfImg] = useState();
     const [actor, setActor] = useState();
+    const [moviesForActor, setMoviesForActor] = useState();
     const [rating, getRating] = useState();
     const [recomendedMovies, setRecomendedMovies] = useState();
     const [loading, setLoading] = useState(false);
-    const [open, setOpen] = useState(false); //alert for Review
-
-    const handleClick = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
-  
-      setOpen(false);
-    };
-
+    const [open, setOpen] = useState(false); 
+    
     useEffect(async () => {
         window.scrollTo(0, 0);
 
-        //add flag for loading signa
+        //add flag for loading sign
         setLoading(true);
 
         //get movie 
         const movieData = await getOneMovie(movieId);
         setMovie(movieData);
-        console.log('movieData:');
-        console.log(movieData);
 
         //get rating from OMDB
         const omdb = await getOMDB(movieData.data.title);
         getRating(omdb);
-        // console.log('omdb');
-        // console.log(omdb);
 
         //get configuration for images
         const imgConf = await getTMDBconf();
         setConfImg(imgConf);
-        // console.log('imgConf');
-        // console.log(imgConf);
 
         //get actors from credits
-        console.log('movieId');
-        console.log(movieId);
-        let credits = await gerCredits(movieId);
-
-        console.log('movieId for the credits (actors)');
-        console.log(credits);
+        const credits = await getCredits(movieId);
         setActor(credits);
 
         //show recomended movies
         const recomendedMoviesData = await recomendations(movieId);
-        // console.log('recomendedMovies');
-        // console.log(recomendedMoviesData);
         setRecomendedMovies(recomendedMoviesData);
-        // console.log(recomendedMovies);
-        // const recomendedMoviesUrl = recomendedMoviesData.data.results.map(movie => movie.backdrop_path);
 
         setLoading(false);
 
     },[movieId]);
+
+    //alert for Review
+    const handleClick = () => {
+        setOpen(true);
+        };
+        const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+        };
+
+    //show movies of a specific actor
+    const showMovies = async (actorId) => {
+        const actorMoviesData = await getMoviesForActor(actorId);
+        setMoviesForActor(actorMoviesData);
+        console.log(actorMoviesData);
+    }
+
+    const removeActorMovies = () => {
+        setMoviesForActor();
+    }
     
     return (
         <>
@@ -99,9 +100,17 @@ function MovieDetails () {
                 {
                     movie &&
                     <div className="detailsPage head-details"> 
-                        <h1>{movie.data.title}</h1>
-                        <h3 className="plot-summery">{movie.data.overview}</h3>
-                        <h2>Ratings</h2>
+                    <Typography variant="h2">
+                        {movie.data.title}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        {movie.data.tagline}
+                    </Typography>
+                        <h4 className="plot-summery"><i>{movie.data.overview}</i></h4>
+                        <h4 className="plot-summery">Released date: {movie.data.release_date}</h4>
+                        <Typography variant="h4">
+                            Ratings
+                        </Typography>
                         {rating && !rating.data.Error && rating.data.Ratings.map((rating, index) => 
                         {
                             return (
@@ -124,7 +133,7 @@ function MovieDetails () {
                         </Button>
                         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
                             <Alert variant="filled" severity="error" >
-                                In Construction
+                                Nobody cares what you think
                             </Alert>
                         </Snackbar>
                     </div>
@@ -146,32 +155,44 @@ function MovieDetails () {
         {
             !loading &&
             <div className="afetrLoading">
-                <h1>Actors:</h1>
+                <h1>Lead Actors:</h1>
                 <div className="detaisPage actors">
-                    {actor && 
-                        actor.data.cast.map((actorUrl, index) => 
-                        {return(
-                            actorUrl.profile_path && 
-                            <Tooltip key={index} title={actorUrl.name} placement="top">
-                                <img key={index} className="oneActor" 
-                                src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${actorUrl.profile_path}`} alt={actorUrl.name} 
-                                />
-                            </Tooltip>
-                            )
-
-                        }).slice(0,5)
-                    }
+                    <div>
+                        {actor && 
+                            actor.data.cast.slice(0,5).map((actorUrl, index) => 
+                            {return(
+                                actorUrl.profile_path && 
+                                <Tooltip key={index} title={actorUrl.name} placement="top">
+                                    <img key={index} className="oneActor" 
+                                    src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${actorUrl.profile_path}`} alt={actorUrl.name} 
+                                    onClick={() => showMovies(actorUrl.id)}
+                                    />
+                                </Tooltip>
+                                )
+                            })
+                        }
+                        <ul>
+                            <h2>do i look familiar to you?</h2>
+                            {
+                                moviesForActor && 
+                                moviesForActor.data.cast.slice(0,5).map(movie => 
+                                <li>{`I played ${movie.character} in `}
+                                    <Link to={`/movie/${movie.id}`} onClick={() => removeActorMovies()}>{movie.original_title}</Link>
+                                </li>)
+                            }
+                        </ul>
+                    </div>
                 </div>
 
                 <hr />
 
                 <h1>You may like also...</h1>
                 <div className="similarMovies">
-                    {recomendedMovies && recomendedMovies.data.results.map((movieUrl, index) => 
+                    {recomendedMovies && recomendedMovies.data.results.slice(0,10).map((movieUrl, index) => 
                     {return(
                     movieUrl.poster_path &&
                         <Route path="/movie/:movieId">
-                            <Link to={`/movie/${movieUrl.id}`}>
+                            <Link to={`/movie/${movieUrl.id}`} onClick={() => removeActorMovies()}>
                                 <img key={index} className="movieSim"  
                                 src={`${confImg.data.images.base_url}/${confImg.data.images.profile_sizes[2]}/${movieUrl.poster_path}`} 
                                 alt={movieUrl.title}
@@ -179,11 +200,10 @@ function MovieDetails () {
                             </Link>
                         </Route>
                     )}
-                    ).slice(0,10)}
+                    )}
                 </div>
             </div>
         }
-
         </>
     )
 }
